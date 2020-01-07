f1e69ae400f7c0d5ed0ce197c68770946ffb561c
Jonathan-A-White <jonathan.jawhite@gmail.com>
2020-01-07 14:11:37 -0500

    Addressing Jon's Comments.

--- global/outputs.tf
+++ global/outputs.tf
@@ -1,8 +1,3 @@
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.terraform_state.arn
-  description = "The ARN of the S3 bucket"
-}
-
 output "dynamodb_table_name" {
   value       = aws_dynamodb_table.terraform_locks.name
   description = "The name of the DynamoDB table"
@@ -17,3 +12,9 @@ output "route53_zone_main" {
   value = aws_route53_zone.main
   description = "The main Route53 Zone object"
 }
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
--- prod/main.tf
+++ prod/main.tf
@@ -3,24 +3,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#data "terraform_remote_state" "global" {
-  #backend = "s3"
-
-  #config = {
-    #bucket = "terraform-white-plains-nac-state" 
-    #key    = "global/s3/terraform.tfstate"
-    #region = "us-east-1"
-  #}
-#}
-
-#variable "root_domain_name" {
-  #default = "mychoir.info"
-#}
-
-#variable "prod_domain_name" {
-  #default = "prod.mychoir.info"
-#}
-
 terraform {
   backend "s3" {
     bucket         = "terraform-white-plains-nac-state"
